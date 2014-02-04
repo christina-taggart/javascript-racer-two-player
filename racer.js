84ef2db207a44e794f25e575b005d2cffcb34a1d
Emily Foley & Patrick Vilhena <pair+emfoley.nifengle@devbootcamp.com>
2014-02-04 11:26:37 -0800

    Added keyup event handler to move players with keys q and p.

--- racer.js
+++ racer.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+  $(document).on('keyup', function(event){
+    if (event.which == 80) {
+      movePlayer(1);
+    }
+    else if (event.which == 81) {
+      movePlayer(2);
+    }
+    }
+    )
+});
+
 var movePlayer = function(playerNum) {
   current_cell = $('#player'+playerNum+'_track .active');
   next_cell = current_cell.next();
