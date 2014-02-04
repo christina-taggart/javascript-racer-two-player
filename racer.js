62814ecc24ffe98e5095d1be3f5a1ef676e06510
Elliot Chalfant & Henry Hsu <pair+epchalfant.henryh28@devbootcamp.com>
2014-02-04 15:27:40 -0800

    Javascript racer now working.

--- racer.js
+++ racer.js
@@ -1,11 +1,25 @@
-// test
 
-// document.addEventListener('keydown', function(event){
-//   if(event.keyCode == 37){
-//     console.log("left was pressed");
-//   }
-//   else if(event.keyCode == 39){
-//       console.log("right was pressed");
-//   }
-// }, true);
-
+$(document).ready(function(){
+  document.addEventListener('keyup', function(event){
+      $(".racer_table").each(function(){
+      if(event.keyCode == 81){
+        var position = $('.active1').next();
+        $('td').removeClass("active1");
+        position.addClass("active1");
+        if(position.hasClass("end active1")){
+          alert("Player 1 wins, resetting game.")
+          location.reload(true);
+        }
+      }
+      else if(event.keyCode == 80){
+        var position = $('.active2').next();
+        $('td').removeClass("active2");
+        position.addClass("active2");
+        if(position.hasClass("end active2")){
+          alert("Player 2 wins, resetting game.")
+          location.reload(true);
+        }
+      }
+    });
+  }, true);
+});
