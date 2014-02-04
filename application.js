862aa02587efae1987e19f2b4e1582cf705bc8c7
Eli Shkurkin <eshkurkin@gmail.com>
2014-02-04 15:38:02 -0800

    Create game play, bound to key presses, but no starting or winning states

--- application.js
+++ application.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-  $(document).on('keyup', function(event) {
-    $('body').css('background-color', 'red');
-    // var update_player_position = function(player, newPosition) {
-    //   var whichCell = document.querySelector(".racer_table").rows[player-1].cells[newPosition];
-    //   var whichPlayer = document.getElementById(player).querySelector(".active");
-    //   $(whichPlayer).removeClass("active");
-    //   $(whichCell).addClass("active");
-    // }
+  var update_player_position = function(player, newPosition) {
+  var whichCell = document.querySelector(".racer_table").rows[player-1].cells[newPosition];
+  var whichPlayer = document.getElementById(player).querySelector(".active");
+    $(whichPlayer).removeClass("active");
+    $(whichCell).addClass("active");
+  }
 
+  var generateRandomNumber = function() {
+    return Math.floor((Math.random()*4)+1);
+  }
+
+  var currentPositionPlayerOne = 0;
+  var currentPositionPlayerTwo = 0;
+  $(document).on('keyup', function() {
+    currentPositionPlayerOne = currentPositionPlayerOne + generateRandomNumber();
+    currentPositionPlayerTwo = currentPositionPlayerTwo + generateRandomNumber();
+    console.log(currentPositionPlayerOne);
+    update_player_position(1, currentPositionPlayerOne);
+    update_player_position(2, currentPositionPlayerTwo);
   });
 });
