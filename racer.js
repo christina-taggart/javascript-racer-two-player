d4d62804fd7ee9204f36c3d261846a1ad374f267
Emily Foley & Patrick Vilhena <pair+emfoley.nifengle@devbootcamp.com>
2014-02-04 11:53:03 -0800

    Restart button and styling changes. Wrapped move and win logic into one method.

--- racer.js
+++ racer.js
@@ -1,18 +1,33 @@
 $(document).ready(function(){
   $(document).on('keyup', function(event){
     if (event.which == 80) {
-      movePlayer(1);
+      playGame(1);
     }
     else if (event.which == 81) {
-      movePlayer(2);
+      playGame(2);
     }
     }
     )
 });
+
+var playGame = function(playerNum){
+  movePlayer(playerNum);
+  checkWinner(playerNum);
+}
 
 var movePlayer = function(playerNum) {
   current_cell = $('#player'+playerNum+'_track .active');
   next_cell = current_cell.next();
   current_cell.removeClass('active');
   next_cell.addClass('active');
+}
+
+var checkWinner = function(playerNum) {
+  if ($('#player'+playerNum+'_track .active').next().length==0){
+    var replay = window.confirm("Player "+playerNum+" wins! Play again?");
+    if (replay)
+    {
+      location.reload(true)
+    }
+  }
 }
