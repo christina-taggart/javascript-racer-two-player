335f050dfd57d37cfd58cf936b4870516e89ef55
Apprentice <apprentice@dbc12.local>
2014-02-04 15:42:54 -0800

    add keypress functionality for both race lanes.

--- source/application.js
+++ source/application.js
@@ -1,13 +1,18 @@
-
-
+$(document).ready(function() {
+  $(document).keyup(function(key) {
+    if(key.keyCode === 81){
+      raceAhead("car1", 10)
+    }
+    if(key.keyCode === 80){
+      raceAhead("car2", 10)
+    }
+  });
+});
 
 
 function raceAhead (car, position) {
   var player = document.getElementsByClassName(car)
   var currentMargin = getComputedStyle(player[0]).marginLeft
-  console.log(currentMargin)
   var newMargin = (position + parseInt(currentMargin)).toString() + "px"
-  console.log(position)
-  console.log(newMargin)
   player[0].style.marginLeft = newMargin
 };
