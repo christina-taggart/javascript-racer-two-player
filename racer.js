581eb40f5aafa44fdb63596fb1673f357ea78c04
Elliot Chalfant & Henry Hsu <pair+epchalfant.henryh28@devbootcamp.com>
2014-02-04 11:11:14 -0800

    made game board html and css, also included basic js to take keyboard input

---
+++ racer.js
@@ -0,0 +1,11 @@
+// test
+
+// document.addEventListener('keydown', function(event){
+//   if(event.keyCode == 37){
+//     console.log("left was pressed");
+//   }
+//   else if(event.keyCode == 39){
+//       console.log("right was pressed");
+//   }
+// }, true);
+
