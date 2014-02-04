8846047c021182d99467423274c11205d3a88716
Christina Taggart & John Olmsted <pair+christina-taggart.qsymmachus@devbootcamp.com>
2014-02-04 12:08:10 -0800

    Saves work before lunch

---
+++ application.js
@@ -0,0 +1,13 @@
+// Document Ready:
+$(function() {
+
+  // Put player ships in active spaces
+  $('#player-1-track .active').append("<img src='images/rocket_usa.png'/>");
+  $('#player-2-track .active').append("<img src='images/rocket_martian.png'/>");
+
+});
+
+
+var update_player_position = function(player, space) {
+
+}
