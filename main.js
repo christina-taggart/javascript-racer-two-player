0aebcacdecb43f96c5f868803515c518141cad5b
Alexander Leishman <null>
2014-02-04 17:40:52 -0800

    Release 3: Reset map when there is a winner

--- main.js
+++ main.js
@@ -18,6 +18,31 @@ Player.prototype.clear = function(){ this.track.find(".active").attr('class', ''
 Player.prototype.display = function(){ $(this.track.children()[this.position]).attr('class', 'active') };
 Player.prototype.update = function(event){
 	if(event.keyCode == this.keyCode){ this.advance() }
+	this.checkWin(20);
 	this.clear();
  	this.display(); 
 };
+
+Player.prototype.checkWin = function(winVal){
+	if (Object.winner || this.position > winVal){ this.reset(); }
+	if(this.position == winVal){
+		$('h1').text("Player " + this.track[0].id[6] + " Wins!");
+		isWinner();
+	}
+};
+
+Player.prototype.reset = function(){
+	noWinner();
+	this.position = 0;
+	this.display();
+};
+
+function isWinner(){
+	Object.winner = true;
+}
+
+function noWinner(){
+	Object.winner = false;
+}
+
+Object.prototype.winner = false;
