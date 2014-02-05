7cecd7609c3a283e69819755abfcfc4da57046a5
Alexander Leishman <null>
2014-02-04 16:43:44 -0800

    Down the rabbit hole...

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 $(function(){
-	var player1 = $("#player1_strip")
-	var player2 = $("#player2_strip")
-	updateTrackPosition(player1, 4)
+	var player1 = new Player($("#player1_strip"), 65);
+	var player2 = new Player($("#player2_strip"), 76);
+	$(document).keyup(function(event){
+		player1.update(event);
+		player2.update(event);
+	});
 });
 
-var updateTrackPosition = function(player, position){
-	var trackPosition = player.children();
-	trackPosition.attr('class', '');
-	$(trackPosition[position - 1]).attr('class', 'active');
+function Player(track_cells, keyCode){
+	this.position = 0,
+	this.track = track_cells,
+	this.keyCode = keyCode
 }
+
+Player.prototype.advance = function(){ this.position += 1 };
+Player.prototype.clear = function(){ this.track.find(".active").attr('class', '') };
+Player.prototype.display = function(){ $(this.track.children()[this.position]).attr('class', 'active') };
+Player.prototype.update = function(event){
+	if(event.keyCode == this.keyCode){ this.advance() }
+	this.clear();
+ 	this.display(); 
+};
