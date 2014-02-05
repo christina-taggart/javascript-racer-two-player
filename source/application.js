c44d30d2501d965064cfc370a2c4d776613189fe
Apprentice <apprentice@dbc12.local>
2014-02-04 17:53:11 -0800

    Add win and reset

--- source/application.js
+++ source/application.js
@@ -1,18 +1,61 @@
 $(document).ready(function() {
   $(document).keyup(function(key) {
-    if(key.keyCode === 81){
-      raceAhead("car1", 10)
+    if (carOne.winning === false && carTwo.winning === false) {
+      if (key.keyCode === 81){
+        raceAhead(carOne, 20)
+      }
+      if (key.keyCode === 80){
+        raceAhead(carTwo, 20)
+      }
     }
-    if(key.keyCode === 80){
-      raceAhead("car2", 10)
-    }
+      if (carOne.margin === 1000){
+        carOne.winning = true,
+        winCondition(carOne)
+      }
+      if (carTwo.margin === 1000){
+        carTwo.winning = true,
+        winCondition(carTwo)
+      }
   });
 });
 
+var carOne = {
+  divName: "car1",
+  carName: "Benz",
+  margin: 0,
+  winning: false
+};
+
+var carTwo = {
+  divName: "car2",
+  carName: "MegaTruck",
+  margin: 0,
+  winning: false
+};
+
 
 function raceAhead (car, position) {
-  var player = document.getElementsByClassName(car)
+  var player = document.getElementsByClassName(car.divName)
   var currentMargin = getComputedStyle(player[0]).marginLeft
   var newMargin = (position + parseInt(currentMargin)).toString() + "px"
   player[0].style.marginLeft = newMargin
+  car.margin += position
 };
+
+function winCondition(victor) {
+  document.getElementById("winner").style.display = "block"
+  document.getElementById("winner_text").innerHTML = victor.carName + " wins! Click button to play again!"
+};
+
+function reset() {
+  location.reload();
+}
+// todo
+// win condition :
+// if currentMargin > x
+// disable racing
+// present :
+// restart option
+// hidden button
+// reveal on win condition
+// resets margins
